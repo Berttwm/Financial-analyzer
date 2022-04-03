c1cd399d296bee39a7b74302f1b9d53c2eee98ed
berttwm <bertrandtwm@gmail.com>
2022-04-04 00:09:51 +0800

    [Init] vsstudio project

---
+++ Header/Puller.h
@@ -0,0 +1,6 @@
+#pragma once
+class Puller
+{
+private:
+	int x;
+};
---
+++ main/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+#include "../Header/Puller.H"
+
+int main()
+{
+	std::cout << "hello" << std::endl;
+	return 0;
+}
