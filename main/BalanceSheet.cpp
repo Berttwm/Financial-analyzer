649dcbc1ba7800fae21f1b2d014cc41ae022b90e
berttwm <bertrandtwm@gmail.com>
2022-07-21 00:58:27 +0800

    [Bugfix] Puller unable to pull bal sheet and cash flow problem fixed

--- main/BalanceSheet.cpp
+++ main/BalanceSheet.cpp
@@ -3,3 +3,8 @@ BalanceSheet::BalanceSheet(rapidjson::Document& d_bal_sheet)
 {
 
 }
+void BalanceSheet::addToMap(IncomeStatementMetrics metric, std::string value)
+{
+	// TODO: Check if need to check if this current mapping is empty.
+	//this->metrics_yearly_map[metric].push_back(value);
+}
--- main/CashFlow.cpp
+++ main/CashFlow.cpp
@@ -3,3 +3,8 @@ CashFlow::CashFlow(rapidjson::Document& d_cash_flow)
 {
 
 }
+void CashFlow::addToMap(IncomeStatementMetrics metric, std::string value)
+{
+	// TODO: Check if need to check if this current mapping is empty.
+	//this->metrics_yearly_map[metric].push_back(value);
+}
