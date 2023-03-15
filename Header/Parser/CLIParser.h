efdf9c826df796635c0bf79353b283fc60128bfb
jicsontoh <jicsontoh@gmail.com>
2023-03-15 21:29:04 +0800

    [Feature] Add Boost lib for CLI

--- Header/Parser/CLIParser.h
+++ Header/Parser/CLIParser.h
@@ -24,14 +24,11 @@ private:
 	const int num_args;
 	char** argv;
 
-	const std::string help = "--help";
+	Scorer* get_scorer(std::string);
 
-	Scorer* get_scorer();
-
-	void parse_help();
-	void parse_entire_stock();
-	void parse_whole_category();
-	void parse_specified_category();
+	void parse_entire_stock(std::string);
+	void parse_whole_category(std::string);
+	void parse_specified_category(std::string);
 
 	void get_metrics_performances(Scorer*, std::string = "");
 	void get_category_scores(Scorer*, std::string = "");
