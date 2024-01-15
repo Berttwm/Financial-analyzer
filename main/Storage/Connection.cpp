f9faf68d3931a928db55004792db4ae490ec120f
joey-chance <joey.chance@u.nus.edu>
2024-01-15 08:30:26 +0800

    Compile done. Fail linker

---
+++ main/Storage/Connection.cpp
@@ -0,0 +1,7 @@
+// #include <sw/redis++/redis++.h>
+// #include <iostream>
+// using namespace sw::redis;
+// int main(void) {
+//     auto redis = Redis("redis://default:WcMt1ynsg0su5zms9tyu4REBSvElkUPO@redis-12168.c295.ap-southeast-1-1.ec2.cloud.redislabs.com:12168");
+//     std::cout << redis.ping() << std::endl;
+// }
